37d80a09a66399a9eba3aa921812d2f9b1b4e14c
Kevin Brothaler <contact@digipom.com>
2016-11-15 17:28:31 -0400

    Add a dummy SDL loop to the emu driver

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,26 +1,51 @@
 extern crate chip8_emu;
-extern crate curl;
+extern crate sdl2;
 
-use std::io::Write;
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
 
 use chip8_emu::Chip8;
 
-use curl::easy::Easy;
+use sdl2::{Sdl};
+use sdl2::pixels::PixelFormatEnum::BGR24;
+use sdl2::render::{Renderer, Texture, TextureAccess};
 
 fn main() {
 	let mut buffer = [0u8; 1536];
-    let mut easy = Easy::new();
-    easy.url("https://github.com/badlogic/chip8/blob/master/roms/pong.rom?raw=true").unwrap();
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+    	println!("Usage: chip8_emu_driver rom");
+    	return;
+    }
 
-    {
-	    let mut transfer = easy.transfer();
-	    transfer.write_function(|data| {
-	    	Ok((&mut buffer[..]).write(data).unwrap())
-	    }).unwrap();
-	    transfer.perform().unwrap();
-	}
+    let mut file = File::open(&args[1]).unwrap();
+    file.read(&mut buffer[..]).unwrap();    
 
     let mut chip8 = Chip8::new_and_init();
     chip8.load_rom(&buffer);
 
+	let sdl = sdl2::init().unwrap();	
+	let mut event_pump = sdl.event_pump().unwrap();
+
+	let video = sdl.video().unwrap();
+	let window = video.window("Chip 8 emu", 640, 320).build().unwrap();
+	let renderer = window.renderer().accelerated().present_vsync().build().unwrap();
+	let texture = renderer.create_texture(BGR24, TextureAccess::Streaming, 640, 320).unwrap();
+	
+	// TODO this burns up 100% of CPU
+	loop {
+		for event in event_pump.poll_iter() {
+			use sdl2::event::Event;
+			match event {
+				Event::Quit { .. } => {
+					// Should tear down everything
+					return;
+				},
+				_ => {
+					// Skip this event.
+				}
+			}
+		}
+	}
 }
